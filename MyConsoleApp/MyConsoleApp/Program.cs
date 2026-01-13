bddf9ed5252a2aaaf388d9d53f281a0a97cb8122
skinofme <skinoffme@gmail.com>
2026-01-13 16:18:52 -0600

    feat: Add Dockerfile to the application
    also application code has been modified a little

--- MyConsoleApp/MyConsoleApp/Program.cs
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Runing app...");
 Console.WriteLine($"Date: {DateTime.Now.ToLongDateString()}");
 Console.WriteLine($"Time: {DateTime.Now.ToLongTimeString()}");
+Console.WriteLine("Executing completed successfully.");
