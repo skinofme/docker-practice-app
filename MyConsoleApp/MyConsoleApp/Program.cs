bc8c69bf36b6285538c0f63d10b02f0394460d8c
skinofme <skinoffme@gmail.com>
2026-01-14 10:49:52 -0600

    feat(App): Add envionment variable to the execution environment

--- MyConsoleApp/MyConsoleApp/Program.cs
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Runing app...");
+﻿var hosted = Environment.GetEnvironmentVariable("HOSTED") ?? "Local"; // HOSTED variable can be changed on environment
+
+Console.WriteLine($"Runing app from {hosted}...");
 Console.WriteLine($"Date: {DateTime.Now.ToLongDateString()}");
 Console.WriteLine($"Time: {DateTime.Now.ToLongTimeString()}");
 Console.WriteLine("Executing completed successfully.");
