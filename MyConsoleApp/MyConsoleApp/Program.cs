5d36525fc7f1654dff29ed8d4fe7c5c338450fb7
skinofme <skinoffme@gmail.com>
2026-01-15 09:51:52 -0600

    feat(App): Add environment selection via args and execution timing

--- MyConsoleApp/MyConsoleApp/Program.cs
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -1,6 +1,19 @@
-﻿var hosted = Environment.GetEnvironmentVariable("HOSTED") ?? "Local"; // HOSTED variable can be changed on environment
+﻿using System.Diagnostics;
 
-Console.WriteLine($"Runing app from {hosted}...");
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+var hosted = Environment.GetEnvironmentVariable("HOSTED") ?? "Local"; // HOSTED variable can be changed on environment
+var configuration = args.Length > 0 ? args[0] : "Development";
+
+
+Console.WriteLine($"Runing app from {hosted}...{Environment.NewLine}");
+Console.WriteLine($"Configuration: {configuration}");
 Console.WriteLine($"Date: {DateTime.Now.ToLongDateString()}");
 Console.WriteLine($"Time: {DateTime.Now.ToLongTimeString()}");
-Console.WriteLine("Executing completed successfully.");
+
+Console.WriteLine($"{Environment.NewLine}Executing completed successfully.");
+
+stopWatch.Stop();
+if(configuration.ToUpper().Equals("DEVELOPMENT") || configuration.ToUpper().Equals("TEST")) 
+	Console.WriteLine($"Execution time: {stopWatch.ElapsedMilliseconds / 100.00}s");
